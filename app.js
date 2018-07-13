2c14757902fc139f7ad05bcd92ffac3753ba8171
Justin Stoddard <justinst@usa-it-b8bg8wp.corp.unicity.net>
2018-07-13 12:20:10 -0600

    Made a req to grab URl

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ var dt = require('./firstModule');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-type': 'text/html'});
     res.write("Hello Justin! the current date and time are currently: " + dt.myDateTime());
+    res.write(req.url);
     res.end();
 }).listen(8080);
